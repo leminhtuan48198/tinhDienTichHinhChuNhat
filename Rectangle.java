4614959a1e4b211b42fe6bf6c526872af79cd1fb
Tuan le <leminhtuan48198@gmail.com>
2022-08-04 11:11:43 +0700

    add file demo

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+package com.codegym;
+import java.util.Scanner;
+public class Rectangle {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scaner.nextFloat();
+    }
+}
